f0e138e3698690636f1fb4065e1b793fd6f61242
Luis Magallon <lmagal01@calpoly.edu>
2025-02-01 15:06:27 -0800

    Engine.java

---
+++ publisher/src/main/java/Engine.java
@@ -0,0 +1,36 @@
+
+/**
+ * This class represents a sample model for the application.
+ * It is executed in a separate thread and updates the blackboard with the current time.
+ *
+ * @author javiergs
+ * @version 1.0
+ */
+public class Engine implements Runnable {
+
+    private boolean wait = true;
+    private boolean stop = false;
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                Thread.sleep(1000);
+                if (!wait) {
+                    Blackboard.getInstance().addValue("time", System.currentTimeMillis());
+                }
+            } catch (InterruptedException e) {
+                // log the error
+            }
+        }
+    }
+
+    public void pause(boolean wait) {
+        this.wait = wait;
+    }
+
+    public void stop(boolean stop) {
+        this.stop = this.stop;
+    }
+
+}
