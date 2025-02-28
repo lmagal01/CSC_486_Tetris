af7ac7564286e7d92598a4f5ba0a4724a77e751f
dragon123al <alin100@calpoly.edu>
2025-02-27 16:33:48 -0800

    Circle

--- subscriber/src/main/java/Main.java
+++ subscriber/src/main/java/Main.java
@@ -76,4 +76,57 @@ public class Main extends JFrame {
         main.setVisible(true);
         main.setTitle("MQTT Subscriber");
     }
+
+
+    // CircleApp
+    private int x = 750, y = 500, diameter = 100;
+    private Color circleColor = Color.RED;
+
+    public void CircleApp() {
+        JFrame frame = new JFrame("Circle Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1800, 900);
+        frame.add(this);
+        frame.setVisible(true);
+    }
+
+    public void changePosition(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+        repaint();
+    }
+
+    public void changeSize(int newDiameter) {
+        this.diameter = newDiameter;
+        repaint();
+    }
+
+    public void changeColor(Color newColor) {
+        this.circleColor = newColor;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(circleColor);
+        g.fillOval(x, y, diameter, diameter);
+    }
+
+    public static void circleMain(String[] args) {
+        CircleApp app = new CircleApp();
+
+        // Example usage
+        try {
+            Thread.sleep(1000);
+            app.changePosition(100, 150);
+            Thread.sleep(1000);
+            app.changeSize(150);
+            Thread.sleep(1000);
+            app.changeColor(Color.BLUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
